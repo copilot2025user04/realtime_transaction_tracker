d605d2335d12e4949725e45d1a4c3ba436ca1925
labuser <bikash.agrawala@in.ey.com>
2025-04-11 22:28:39 +0000

    stash1

---
+++ src/main/java/com/ey/bank/realtime_transaction_tracker/RealtimeTransactionTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ey.bank.realtime_transaction_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeTransactionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeTransactionTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ey/bank/realtime_transaction_tracker/RealtimeTransactionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.bank.realtime_transaction_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeTransactionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
